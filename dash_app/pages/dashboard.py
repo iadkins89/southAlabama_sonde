cd5797df866d063ee28cb0917a560904da9ab25d
Ian Adkins <ian.adkins1989@gmail.com>
2024-11-18 18:27:54 -0600

    Attempt to make dashboard more mobile friendly

--- dash_app/pages/dashboard.py
+++ dash_app/pages/dashboard.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 from datetime import datetime
 from dash_extensions import EventSource
 from server.models import get_unique_sensor_names
+import pytz
 
 register_page(
 	__name__,
@@ -12,7 +13,10 @@ register_page(
 
 
 def layout():
+    cst = pytz.timezone('America/Chicago')
+    cst_today = datetime.now(cst).replace(hour=0, minute=0, second=0, microsecond=0)
     sensor_options = [name for name in get_unique_sensor_names()]
+
     layout = dbc.Container([
         dbc.Row([
             dbc.Col(dbc.Form([
@@ -21,8 +25,8 @@ def layout():
                 dcc.DatePickerRange(
                     id='date-picker-range',
                     minimum_nights=0,
-                    start_date=datetime.today(),
-                    end_date=datetime.today(),
+                    start_date=cst_today,
+                    end_date=cst_today,
                     stay_open_on_select=True,
                     style={"margin-left": "15px",
                            'padding': '10px'}
@@ -62,7 +66,7 @@ def layout():
                     'border': 'none',  # Remove border
                     'border-color': 'rgba(0,0,0,0)',
                     'color': 'rgba(0,0,0,0)'}
-            }), style={'height': '100%', 'width': '100%'})
+            }), xs=12, sm=10, md=10, lg=8 ),
         ]),
         EventSource(id='eventsource', url='/eventsource')
     ])
