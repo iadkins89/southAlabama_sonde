7c0ce3d6effc6d47108a8a741fe302fc1827542e
Ian Adkins <ian.adkins1989@gmail.com>
2024-11-18 18:13:34 -0600

    Changed About page to be more mobile friendly

--- dash_app/pages/about.py
+++ dash_app/pages/about.py
@@ -12,7 +12,7 @@ def layout():
 	[
 		dbc.Row(
 			dbc.Col(
-				html.H1("About Us", style={"margin-top": "35px", "margin-left": "190px", "margin-right": "190px"} ),
+				html.H1("About Us", style={"margin-top": "2.7%", "margin-left": "14.5%", "margin-right": "14.5%"}),
 			width=12
 			)
 	),
@@ -23,14 +23,14 @@ def layout():
 					"Our mission is to innovate and implement cutting-edge telemetry devices designed to monitor and protect our coastal environments. "
 					"Utilizing the advanced capabilities of LoRaWAN (Long Range Wide Area Network) technology, our devices provide real-time data collection and transmission over vast distances, making them ideal for remote and hard-to-reach coastal areas.",
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"} 
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 		width=12
 		)
 	),
 	dbc.Row(
 		dbc.Col(
-			html.H2("Our Mission", style={"margin-left": "190px", "margin-right": "190px"} ),
+			html.H2("Our Mission", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
 			width=12
 		)
 	),
@@ -39,7 +39,7 @@ def layout():
 				html.P(
 				"Our primary goal is to enhance the understanding and preservation of coastal ecosystems through accurate and reliable telemetry. By deploying these advanced devices, we aim to:",
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"}
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 			width=12
 		)
@@ -48,10 +48,10 @@ def layout():
 		dbc.Col(
 			html.Ul(
 			[
-				html.Li("Monitor environmental parameters such as water quality, temperature, and salinity.", style={"margin-left": "190px", "margin-right": "190px"} ),
-				html.Li("Track the movement and behavior of marine and coastal wildlife.", style={"margin-left": "190px", "margin-right": "190px"} ),
-				html.Li("Support scientific research and conservation efforts with high-quality data.", style={"margin-left": "190px", "margin-right": "190px"} ),
-				html.Li("Provide valuable information to stakeholders, including researchers, environmental agencies, and the general public.", style={"margin-left": "190px", "margin-right": "190px"} ),
+				html.Li("Monitor environmental parameters such as water quality, temperature, and salinity.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
+				html.Li("Track the movement and behavior of marine and coastal wildlife.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
+				html.Li("Support scientific research and conservation efforts with high-quality data.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
+				html.Li("Provide valuable information to stakeholders, including researchers, environmental agencies, and the general public.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
 			]
 		),
 		width=12
@@ -59,7 +59,7 @@ def layout():
 	),
 	dbc.Row(
 		dbc.Col(
-			html.H2("Our Technology", style={"margin-left": "190px", "margin-right": "190px"} ),
+			html.H2("Our Technology", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
 			width=12
 		)
 	),
@@ -68,7 +68,7 @@ def layout():
 			html.P(
 				"Our telemetry devices leverage LoRaWAN technology to offer:",
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"}
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 		width=12
 		)
@@ -77,9 +77,9 @@ def layout():
 		dbc.Col(
 			html.Ul(
 					[
-						html.Li("Long-Range Communication: Capable of transmitting data over several kilometers, ensuring coverage in remote areas.", style={"margin-left": "190px", "margin-right": "190px"} ),
-						html.Li("Low Power Consumption: Designed to operate for extended periods without the need for frequent battery replacements.", style={"margin-left": "190px", "margin-right": "190px"} ),
-						html.Li("Scalability: Easily expandable networks to accommodate additional sensors and devices.", style={"margin-left": "190px", "margin-right": "190px"} ),
+						html.Li("Long-Range Communication: Capable of transmitting data over several kilometers, ensuring coverage in remote areas.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
+						html.Li("Low Power Consumption: Designed to operate for extended periods without the need for frequent battery replacements.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
+						html.Li("Scalability: Easily expandable networks to accommodate additional sensors and devices.", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
 					]
 			),
 			width=12
@@ -87,7 +87,7 @@ def layout():
 	),
 	dbc.Row(
 		dbc.Col(
-			html.H2("Get Involved", style={"margin-left": "190px", "margin-right": "190px"} ),
+			html.H2("Get Involved", tyle={"margin-left": "14.5%", "margin-right": "14.5%"}),
 			width=12
 		)
 	),
@@ -96,14 +96,14 @@ def layout():
 			html.P(
 				"We invite students, researchers, and anyone passionate about environmental conservation to get involved with our project. Whether you're interested in field deployment, data analysis, or technological development, there's a place for you in our team.",
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"}
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 		width=12
 		)
 	),
 	dbc.Row(
 		dbc.Col(
-			html.H2("Contact Us", style={"margin-left": "190px", "margin-right": "190px"} ),
+			html.H2("Contact Us", style={"margin-left": "14.5%", "margin-right": "14.5%"}),
 			width=12
 		)
 	),
@@ -112,7 +112,7 @@ def layout():
 			html.P(
 				"For more information about our project, collaborations, or any inquiries, please feel free to reach out to us at:",
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"}
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 		width=12
 		)
@@ -125,7 +125,7 @@ def layout():
 					html.A("iea2021@jagmail.southalabama.edu", href="mailto:iea2021@jagmail.southalabama.edu"),
 				],
 				className="lead",
-				style={"margin-left": "190px", "margin-right": "190px"}
+				style={"margin-left": "14.5%", "margin-right": "14.5%"}
 			),
 			width=12
 		)
@@ -138,7 +138,7 @@ def layout():
 					html.A("Telemetry Dashboard GitHub Repository", href="https://github.com/iadkins89/southAlabama_sonde"),
 					],
 					className="lead",
-					style={"margin-left": "190px", "margin-right": "190px"}
+					style={"margin-left": "14.5%", "margin-right": "14.5%"}
 				),
 			width=12
 		)
